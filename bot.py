54e2b111758150071fa73faef16d3774e3575f4e
Datan <Datan.ctupide@yandex.ru>
2026-05-05 13:24:37 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import telebot
 import random
 TOKEN = '8667196115:AAE4JtvInpOgYsE9c0KO-fm7ONi-Oa1Rb8U'
-bot = telebot.Telebot(TOKEN)
-game_inventory = ['m', 'up', 'd', 'l', 'lup', 'ld', 'r', 'rup', 'rd']
+bot = telebot.TeleBot(TOKEN)
+game_inventory = ['Да', 'Нарвное...', 'Возможно', 'Я скучаю по своей жене...', 'Нет', 'Я так не думаю', 'эээээээмм... хз', 'Кто здесь?', 'Не думаю']
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Играть будем в крестик нолик, и это не обсуждается")
+    bot.send_message(message.chat.id, "Скажи мне что то и отвечу")
 
 @bot.message_handler(commands=['end'])
 def end(message):
@@ -14,120 +14,37 @@ def end(message):
 
 @bot.message_handler(content_types=['text'])
 def echo(message):
-    choice_player = message.text.lower()
-    if choice_player in game_inventory:
-        choice_bot = random.choice(game_inventory)
-        if choice_player == 'm':
-            if choice_bot == 'l':
+    choice_player = input()
+    choice_bot = random.choice(game_inventory)
+    x = 0
+    while x == 10:
+            if choice_bot == 'Да':
                 bot.send_message(message.chat.id, choice_bot)
-                bot.send_message(message.chat.id, " | | \n"
-                                                  "O|X| \n"
-                                                  " | | \n")
-                if choice_player == 'lup':
-                    if choice_bot == 'ld':
-                        bot.send_message(message.chat.id, choice_bot)
-                        bot.send_message(message.chat.id, "X| | \n"
-                                                          "O|X| \n"
-                                                          "O| | \n")
-                        if choice_player == 'r':
-                            if choice_bot == 'd':
-                                bot.send_message(message.chat.id, choice_bot)
-                                bot.send_message(message.chat.id, "X| | \n"
-                                                                  "O|X|X\n"
-                                                                  "O|O| \n")
-                                if choice_player == 'rup':
-                                    if choice_bot == 'rd':
-                                        bot.send_message(message.chat.id, choice_bot)
-                                        bot.send_message(message.chat.id, "X| |X\n"
-                                                                          "O|X|X\n"
-                                                                          "O|O|O\n")
-                                        bot.send_message(message.chat.id, "А я победил? Я ПОБЕДИЛ ХАХАХХАХАХАХ")
-                                    elif choice_bot == 'up':
-                                        bot.send_message(message.chat.id, choice_bot)
-                                        bot.send_message(message.chat.id, "X|O|X\n"
-                                                                          "O|X|X\n"
-                                                                          "O|O| \n")
-                                        if choice_player == 'rd':
-                                            bot.send_message(message.chat.id, choice_bot)
-                                            bot.send_message(message.chat.id, "X|O|X\n"
-                                                                              "O|X|X\n"
-                                                                              "O|O|X\n")
-                                            bot.send_message(message.chat.id, "Вы победили мистер свин... вонючка")
-                            elif choice_bot == 'up':
-                                bot.send_message(message.chat.id, choice_bot)
-                                bot.send_message(message.chat.id, "X|O| \n"
-                                                                  "O|X|X\n"
-                                                                  "O| | \n")
-                            elif choice_bot == 'rup':
-                                bot.send_message(message.chat.id, choice_bot)
-                                bot.send_message(message.chat.id, "X| |O\n"
-                                                                  "O|X|X\n"
-                                                                  "O| | \n")
-                            elif choice_bot == 'rd':
-                                bot.send_message(message.chat.id, choice_bot)
-                                bot.send_message(message.chat.id, "X| |O\n"
-                                                                  "O|X|X\n"
-                                                                  "O| | \n")
-                        elif choice_player == 'rd':
-                            bot.send_message(message.chat.id, choice_bot)
-                            bot.send_message(message.chat.id, "X| | \n"
-                                                              "O|X| \n"
-                                                              "O| |X\n")
-                            bot.send_message(message.chat.id, "Я МИСКЛИКНУЛ ЧЕРТ")
-                    elif choice_bot == 'd':
-                        bot.send_message(message.chat.id, choice_bot)
-                        bot.send_message(message.chat.id, "X| | \n"
-                                                          "O|X| \n"
-                                                          " |O| \n")
-                    elif choice_bot == 'up':
-                        bot.send_message(message.chat.id, choice_bot)
-                        bot.send_message(message.chat.id, "X|O| \n"
-                                                          "O|X| \n"
-                                                          " | | \n")
-                    elif choice_bot == 'rup':
-                        bot.send_message(message.chat.id, choice_bot)
-                        bot.send_message(message.chat.id, "X| |O\n"
-                                                          "O|X| \n"
-                                                          " | | \n")
-                    elif choice_bot == 'r':
-                        bot.send_message(message.chat.id, choice_bot)
-                        bot.send_message(message.chat.id, "X| | \n"
-                                                          "O|X|O\n"
-                                                          " | | \n")
-            elif choice_bot == 'lup':
+                bot.send_message(message.chat.id, 'Да')
+            elif choice_bot == 'Нарвное...':
                 bot.send_message(message.chat.id, choice_bot)
-                bot.send_message(message.chat.id, "O| | \n"
-                                                  " |X| \n"
-                                                  " | | \n")
-            elif choice_bot == 'ld':
+                bot.send_message(message.chat.id, 'Нарвное...')
+            elif choice_bot == 'Возможно':
                 bot.send_message(message.chat.id, choice_bot)
-                bot.send_message(message.chat.id, " | | \n"
-                                                  " |X| \n"
-                                                  "O| | \n")
-            elif choice_bot == 'up':
+                bot.send_message(message.chat.id, 'Возможно')
+            elif choice_bot == 'Я скучаю по своей жене...':
                 bot.send_message(message.chat.id, choice_bot)
-                bot.send_message(message.chat.id, " |O| \n"
-                                                  " |X| \n"
-                                                  " | | \n")
-            elif choice_bot == 'd':
+                bot.send_message(message.chat.id, 'Я скучаю по своей жене...')
+            elif choice_bot == 'Нет':
                 bot.send_message(message.chat.id, choice_bot)
-                bot.send_message(message.chat.id, " | | \n"
-                                                  " |X| \n"
-                                                  " |O| \n")
-            elif choice_bot == 'rup':
+                bot.send_message(message.chat.id, 'Нет')
+            elif choice_bot == 'Я так не думаю':
                 bot.send_message(message.chat.id, choice_bot)
-                bot.send_message(message.chat.id, " | |O\n"
-                                                  " |X| \n"
-                                                  " | | \n")
-            elif choice_bot == 'r':
+                bot.send_message(message.chat.id, 'Я так не думаю')
+            elif choice_bot == 'эээээээмм... хз':
                 bot.send_message(message.chat.id, choice_bot)
-                bot.send_message(message.chat.id, " | | \n"
-                                                  " |X|O\n"
-                                                  " | | \n")
-            elif choice_bot == 'rd':
+                bot.send_message(message.chat.id, 'эээээээмм... хз')
+            elif choice_bot == 'Кто здесь?':
                 bot.send_message(message.chat.id, choice_bot)
-                bot.send_message(message.chat.id, " | | \n"
-                                                  " |X| \n"
-                                                  " | |O\n")
+                bot.send_message(message.chat.id, 'Кто здесь?')
+            elif choice_bot == 'Не думаю':
+                bot.send_message(message.chat.id, choice_bot)
+                bot.send_message(message.chat.id, 'Не думаю')
+            x += 1
 
 bot.polling()
